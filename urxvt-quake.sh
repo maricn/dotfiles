a05f6e409a0db113fcbfc24ce6a68af00f403d83
Nikola Maric <3995223+maricn@users.noreply.github.com>
2019-12-12 16:26:00 +0100

    Add .local/bin/urxvtc for urxvt quake

--- urxvt-quake.sh
+++ urxvt-quake.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-wid=`xdotool search --classname urxvtq | head -n 1`
+wid=$(xdotool search --classname urxvtq | head -n 1)
 if [ -z "$wid" ]; then
-   /usr/bin/urxvt -geometry 320x34+0+0 -name urxvtq -depth 32 -b rgba:1111/1111/1111/aaaa
+   ## urxvt -geometry 320x34+0+0 -name urxvtq -depth 32 -b rgba:1111/1111/1111/aaaa
+   ~/.local/bin/urxvtc -geometry 320x34+0+0 -name urxvtq -depth 32 -b rgba:1111/1111/1111/aaaa
    wid=$(xdotool search --classname urxvtq | head -n 1)
    xdotool windowfocus $wid
+   ## xdotool key Control_L+l
 else
     if [ -z "$(xdotool search --onlyvisible --classname urxvtq 2>/dev/null | head -n 1)" ];     then
         xdotool windowmap $wid
