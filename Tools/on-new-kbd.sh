4706aa2d2ba97502069072773b6bc4eeadfcbcc3
Nikola Maric <3995223+maricn@users.noreply.github.com>
2020-06-17 20:56:40 +0200

    fix: Remap keyboard on TADA/BDN with sleep b4
    * Make keyboard remapping a bit safer and with some sleeps

--- Tools/on-new-kbd.sh
+++ Tools/on-new-kbd.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-echo >&2 "$@"
-event=$1 id=$2 type=$3
+echo >&2 "inputplug $@"
+# #!/bin/bash necessary for ${@:4}
+event=$1 id=$2 type=$3 name="${@:4}"
 
-case "$event $type" in
-    'XIDeviceEnabled XISlaveKeyboard')
-        sleep 1 && "$HOME/.xinitrc-keyboard-remap"
+case "$event $type $name" in
+    'XIDeviceEnabled XISlaveKeyboard TADA TADA68 Keyboard' | \
+    'XIDeviceEnabled XISlaveKeyboard Keebio BDN9 Rev. 1')
+        echo "inputplug $name: $event $type"
+        sleep 0.5 && "$HOME/.xinitrc-keyboard-remap"
+    ;;
 esac
