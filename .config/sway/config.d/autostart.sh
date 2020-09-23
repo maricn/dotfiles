4e89378ff1bec6f9c050735508d4ea4bc54da3f2
Nikola Maric <3995223+maricn@users.noreply.github.com>
2020-09-23 17:30:18 +0200

    feat: swaylock, natural scroll, float windows..
    * wofi emojipicker
    * multimonitor setup bugfix when another display port based display is used
    * flameshot with selection (using `slurp`) on `$mod+Shift+P`
    * startup with `gammastep` for late night color correction
    * extended sleep time before startup apps to let sway properly init

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -1,10 +1,15 @@
 exec pulseaudio --start
 exec kanshi &
 exec flashfocus &
+exec gammastep &
 exec pasystray --volume-max 100 --volume-inc 3 --notify all &
 
-exec copyq &
-exec keepassxc &
-exec signal-desktop &
+exec sleep 1 && unset QT_QPA_PLATFORMTHEME && env WAYLAND_DEBUG=1 chromium &
+
+exec sleep 3 && unset QT_QPA_PLATFORMTHEME && env WAYLAND_DEBUG=1 QT_QPA_PLATFORM=xcb copyq &
+exec sleep 3 && keepassxc &
+exec sleep 3 && signal-desktop-beta &
+#exec sleep 3 && whatsapp-for-linux &
+exec sleep 3 && unset QT_QPA_PLATFORMTHEME && env WAYLAND_DEBUG=1 firefox &
 
 #xrdb -merge ~/.Xresources
