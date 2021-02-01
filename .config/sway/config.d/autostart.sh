ae2a3a5d1ab42bd39da95aa11bf9444ccc2a7965
Nikola Maric <3995223+maricn@users.noreply.github.com>
2021-02-01 12:04:13 +0100

    chore: remove autostart libinput-gestures

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -4,8 +4,6 @@ exec brightnessctl --device='tpacpi::kbd_backlight' s 1
 
 exec sleep 1 && pulseaudio --start
 exec sleep 1 && flashfocus &
-exec libinput-gestures-setup stop; \
-    sleep 1 && dex ~/.config/autostart/libinput-gestures.desktop
 
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && chromium &
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && firefox &
@@ -14,5 +12,9 @@ exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=xcb copyq &
 exec sleep 3 && keepassxc &
 exec sleep 3 && signal-desktop-beta &
 #exec sleep 3 && whatsapp-for-linux &
+
+# This just won't work, must start it manually (maybe try with some envvars in the future)
+# exec libinput-gestures-setup stop; \
+    # sleep 5 && libinput-gestures-setup start
 
 #xrdb -merge ~/.Xresources
