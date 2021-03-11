c151b08f528029357d51a3f71b8c953910558cdb
Nikola Maric <3995223+maricn@users.noreply.github.com>
2021-03-11 22:23:45 +0100

    feat: curltime.sh - curl to measure access times

---
+++ Tools/curltime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -w @- -o /dev/null -s "$@" <<'EOF'
+      response_code:  %{response_code}\n
+       redirect_url:  %{redirect_url}\n
+    time_namelookup:  %{time_namelookup}\n
+       time_connect:  %{time_connect}\n
+    time_appconnect:  %{time_appconnect}\n
+   time_pretransfer:  %{time_pretransfer}\n
+      time_redirect:  %{time_redirect}\n
+ time_starttransfer:  %{time_starttransfer}\n
+                    ----------\n
+         time_total:  %{time_total}\n
+EOF
