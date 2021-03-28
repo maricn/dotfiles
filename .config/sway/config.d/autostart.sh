a2b1d661c748f994311e34b14605128d85c390c9
Nikola Maric <3995223+maricn@users.noreply.github.com>
2021-03-28 22:16:52 +0200

    feat: copyq - wayland backend, ydotool instapaste
    * pulseaudio - try to fix failing pulseaudio startup by delaying it for another second
    * swaywm - reduce container title font size

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -2,13 +2,14 @@ exec kanshi &
 exec gammastep &
 exec brightnessctl --device='tpacpi::kbd_backlight' s 1
 
-exec sleep 1 && pulseaudio --start
 exec sleep 1 && flashfocus &
 
+exec sleep 2 && pulseaudio --start
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && chromium &
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && firefox &
 
-exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=xcb copyq &
+exec sleep 3 && ydotoold &
+exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=wayland copyq &
 exec sleep 3 && keepassxc &
 exec sleep 3 && signal-desktop-beta &
 exec sleep 3 && emote &
