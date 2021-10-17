57fae171287724e0ec163f8cd3197fec741467dc
Nikola Marić <3995223+maricn@users.noreply.github.com>
2021-10-17 12:56:47 +0200

    feat: sway - various feats, fixes
    * bindsym $mod+shift+a focus child (opposite of $mod+a - focus parent)
    * mpv not sticky window anymore
    * swaylock - reduce grace to attempt tackling issue when lock is ignored after lid closed; add fade-in 1s
    * input devices - logitech USB mouse, Tiags magic mouse
    * autostart.sh - +mako; brightnessctl in background; aw-server & aw-watcher-window-wayland; libinput-gestures delayed to 30s

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -1,6 +1,7 @@
 exec kanshi &
 exec gammastep &
-exec brightnessctl --device='tpacpi::kbd_backlight' s 1
+exec mako &
+exec brightnessctl --device='tpacpi::kbd_backlight' s 1 &
 
 exec sleep 1 && flashfocus &
 
@@ -13,10 +14,14 @@ exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=wayland copyq &
 exec sleep 3 && keepassxc &
 exec sleep 3 && signal-desktop-beta &
 exec sleep 3 && emote &
+exec sleep 3 && aw-server &
+
 #exec sleep 3 && whatsapp-for-linux &
 
 # This just won't work, must start it manually (maybe try with some envvars in the future)
 # exec libinput-gestures-setup stop; \
-    # sleep 5 && libinput-gestures-setup start
+# trying again with restart instead of start (2021-04-29T12:48:39+0200)
+exec sleep 30 && libinput-gestures-setup restart
 
+exec sleep 7 && aw-watcher-window-wayland &
 #xrdb -merge ~/.Xresources
