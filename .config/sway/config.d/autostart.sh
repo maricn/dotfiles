898398080022c7d30a7445ab828b9aec7ba914ab
Nikola Marić <3995223+maricn@users.noreply.github.com>
2022-10-27 11:51:11 +0200

    feat: swaywm config updated
    * color: lila
    * font: bigger
    * emoji picker: rofi
    * lock screen: refactored command
    * floating windows: added more apps
    * updated API of swaywm - assigning windows to desktops, bindsyms
    * mod+shift+R - color picker
    * mod+shift+M - meeting mode with dual displays
    * autostart - sworkstyle, gammastep-indicator applet delayed, run festival engine
    * zoom configuration for windows

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -1,27 +1,28 @@
 exec kanshi &
-exec gammastep &
 exec mako &
 exec brightnessctl --device='tpacpi::kbd_backlight' s 1 &
 
 exec sleep 1 && flashfocus &
+exec sleep 1 && keepassxc &
 
-exec sleep 2 && pulseaudio --start
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && chromium &
 exec sleep 2 && unset QT_QPA_PLATFORMTHEME && firefox &
 
 exec sleep 3 && ydotoold &
 exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=wayland copyq &
-exec sleep 3 && keepassxc &
 exec sleep 3 && signal-desktop-beta &
 exec sleep 3 && emote &
 exec sleep 3 && aw-server &
+exec sleep 3 && sworkstyle &
+exec sleep 3 && gammastep-indicator &
 
 #exec sleep 3 && whatsapp-for-linux &
 
 # This just won't work, must start it manually (maybe try with some envvars in the future)
 # exec libinput-gestures-setup stop; \
 # trying again with restart instead of start (2021-04-29T12:48:39+0200)
-exec sleep 30 && libinput-gestures-setup restart
+exec sleep 20 && libinput-gestures-setup restart
 
 exec sleep 7 && aw-watcher-window-wayland &
+exec sleep 7 && festival --server &
 #xrdb -merge ~/.Xresources
