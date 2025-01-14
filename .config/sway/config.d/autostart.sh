4120b739b829003ea28e5f6142d2bdb86687c11a
Nikola Marić <3995223+maricn@users.noreply.github.com>
2025-01-14 15:56:35 +0100

    feat: sway - no suspend, kbd alt-shift, scroll
    * way-displays instead of kanshi, when starting up on a projector, don't autostart gammastep
    * aw-server & aw-watcher-window-wayland not used much, disabled

--- .config/sway/config.d/autostart.sh
+++ .config/sway/config.d/autostart.sh
@@ -1,5 +1,12 @@
-exec kanshi &
+# replaced by way-displays
+# exec kanshi &
+exec way-displays > /tmp/way-displays.${XDG_VTNR}.${USER}.log 2>&1
+exec "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" &
+
+# notifications
 exec mako &
+
+
 exec brightnessctl --device='tpacpi::kbd_backlight' s 1 &
 
 exec sleep 1 && flashfocus &
@@ -10,11 +17,13 @@ exec sleep 2 && unset QT_QPA_PLATFORMTHEME && firefox &
 
 exec sleep 3 && ydotoold &
 exec sleep 3 && unset QT_QPA_PLATFORMTHEME && QT_QPA_PLATFORM=wayland copyq &
-exec sleep 3 && signal-desktop-beta &
+exec sleep 3 && signal-desktop &
 exec sleep 3 && emote &
-exec sleep 3 && aw-server &
+# exec sleep 3 && aw-server &
 exec sleep 3 && sworkstyle &
-exec sleep 3 && gammastep-indicator &
+exec sleep 3 && bluetooth-autoconnect -d &
+# no gammastep if the BenQ projector is connected on startup
+exec sleep 3 && way-displays -g | grep -q "BenQ PJ" && gammastep-indicator &
 
 #exec sleep 3 && whatsapp-for-linux &
 
@@ -23,6 +32,6 @@ exec sleep 3 && gammastep-indicator &
 # trying again with restart instead of start (2021-04-29T12:48:39+0200)
 exec sleep 20 && libinput-gestures-setup restart
 
-exec sleep 7 && aw-watcher-window-wayland &
+# exec sleep 7 && aw-watcher-window-wayland &
 exec sleep 7 && festival --server &
 #xrdb -merge ~/.Xresources
